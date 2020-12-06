1a607ddc7c47864256fa71e00e6e2f349d1c6d9d
Himmelzkind <73137185+Himmelzkind@users.noreply.github.com>
2020-12-06 20:44:12 +0100

    Created new function for timestemp
    Function returns current weekday plus local time

--- js/app.js
+++ js/app.js
@@ -1,10 +1,34 @@
+function formatDate(timestemp){
+//calculate the date
+let date = new Date();
+  let weekDays = [ "Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = weekDays[date.getDay()];
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes.toString().length == 1) {
+    minutes = "0" + minutes;
+  }
+  let time = `${hour}:${minutes}`;
+  
+  let formattedDate = `${day} ${time}`;
+  return formattedDate;
+}
+
+let currentTime = new Date();
+let h2 = document.querySelector("h2");
+let formattedDate = formatDate(currentTime);
+h2.innerHTML = `${formattedDate}`;
+
+
 function displayTemperature(response){
+    //exctract temperature from api
     console.log(response.data);
     debugger
     let h1 = document.querySelector("h1").innerHTML= response.data.name;
     let temperatureElement = document.querySelector("#currentTemp");
     let descriptionElement = document.querySelector("#w-description");
     let humidityElement = document.querySelector("#humidity-wind");
+    let h2 = document.querySelector("h2");
     
     let temperatureMax = Math.round(response.data.main.temp_max);
     let temperatureMin = Math.round(response.data.main.temp_min);
